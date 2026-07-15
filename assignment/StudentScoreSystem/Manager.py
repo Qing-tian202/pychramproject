d46d89991ab0c980b60460e9fc65b9c5dc69a07e
Qing-tian202 <zhousong1001@hotmail.com>
2026-07-15 18:38:38 +0800

    软件测试作业：学生管理系统面向对象实现

--- assignment/StudentScoreSystem/Manager.py
+++ assignment/StudentScoreSystem/Manager.py
@@ -274,6 +274,7 @@ class Manager:
 
                 elif op in ["c", " 7"]:
                     n = self.count_students()
+                    print(f"学生数据：{n} ")
                     self.logger.info(f"成功统计到{n}条数据")
 
                 elif op in ["p", "8"]:

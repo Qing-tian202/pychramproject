e599fe34d7338e33710a3714b23d4055467d8a3d
Qing-tian202 <zhousong1001@hotmail.com>
2026-08-26 16:30:44 +0800

    python requests + pytest + allure 接口自动化

--- WeFinance/tools/__init__.py
+++ WeFinance/tools/__init__.py
@@ -2,5 +2,6 @@
 from .tool import BASE_DIR
 from .tool import WF_HOST
 from .Logger import setup_logging
+from .DBTool import DBTool
 
-__all__ = ['BASE_DIR','WF_HOST','setup_logging']
+__all__ = ['BASE_DIR','WF_HOST','setup_logging','DBTool']

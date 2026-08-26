9c25eb8c98ae90f013b906cfb5971b5f509526cf
Qing-tian202 <zhousong1001@hotmail.com>
2026-08-26 16:27:38 +0800

    python selenium Web UI 自动化

--- TPshop/tools/__init__.py
+++ TPshop/tools/__init__.py
@@ -2,6 +2,7 @@
 
 from .Logger import setup_logging
 from .tool import *
+from .DBTool import *
 
 # 导出常用路径
-__all__ = ['BASE_DIR', 'DATA_DIR', 'LOGS_DIR', 'RESULTS_DIR', 'ALLURE_DIR', 'TP_HOST','setup_logging']
+__all__ = ['BASE_DIR', 'DATA_DIR', 'LOGS_DIR', 'RESULTS_DIR', 'ALLURE_DIR', 'TP_HOST','setup_logging', 'DBTool']
